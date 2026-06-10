bd9dbca3a541b3110de3bd40d8366f5eccbfad2d
Manishkenguva <100593586+MANISHKENGUVA@users.noreply.github.com>
2026-06-10 10:41:04 +0000

    feat: bug

--- GOPRO/src/global.js
+++ GOPRO/src/global.js
@@ -9,21 +9,21 @@ export const runWFEngine = () => {
   const configCollection = GETCONFIGCOLLECTION();
   console.log('loanCollection', loanCollection);
   console.log('configCollection', configCollection);
-if (configCollection && !Object.keys(loanCollection.loanEVENTS || {}).length) {
+  
+  if (configCollection && !Object.keys(loanCollection.loanEVENTS || {}).length) {
     console.log('No events found, initializing with config');
     console.log('Config collection:', configCollection?.startState);
-    configCollection?.startState ;
-BORROWER-DETAILS-V1-PERSONAL-INFO-V1
-
-Actor: BORROWER
-Module:DETAILS
-NodeVersion:V1
-StageNode: PERSONAL-INFO
-StageNodeVersion:V1
+    
+    // BORROWER-DETAILS-V1-PERSONAL-INFO-V1
+    // Actor: BORROWER
+    // Module: DETAILS
+    // NodeVersion: V1
+    // StageNode: PERSONAL-INFO
+    // StageNodeVersion: V1
     
     const payLoad = {
       node: configCollection?.startState,
-      nodeResolved:
+      nodeResolved: null
     };
   }
-}
+};
